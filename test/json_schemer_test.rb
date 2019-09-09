43e84e33c62b2fccde59acede314184e46e3df40
Clive Crous <clive.crous@xneelo.com>
2019-09-09 15:09:34 +0200

    Fix an issue where a property is set to a boolean value (can have any values) but a crash results

--- test/json_schemer_test.rb
+++ test/json_schemer_test.rb
@@ -129,6 +129,36 @@ class JSONSchemerTest < Minitest::Test
     }
   end
 
+  def test_it_does_not_fail_using_insert_defaults_when_properties_containe_a_boolean_defined_property
+    schema = {
+      'properties' => {
+        'a' => true
+      }
+    }
+    data = {
+      'a' => 1
+    }
+    assert JSONSchemer.schema(schema, insert_property_defaults: true).valid?(data)
+    assert data == {
+      'a' => 1
+    }
+  end
+
+  def test_it_does_not_fail_using_insert_defaults_when_properties_containe_a_boolean_defined_property_that_does_not_exist
+    schema = {
+      'properties' => {
+        'b' => true
+      }
+    }
+    data = {
+      'a' => 1
+    }
+    assert JSONSchemer.schema(schema, insert_property_defaults: true).valid?(data)
+    assert data == {
+      'a' => 1,
+    }
+  end
+
   def test_it_allows_disabling_format
     schema = JSONSchemer.schema(
       { 'format' => 'email' },
